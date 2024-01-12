4a6be4d9c90796d3bcf93bc28ecd539659c8cf7a
peterg-xd <84744702+peterg-xd@users.noreply.github.com>
2024-01-12 16:54:18 -0500

    Add files via upload

---
+++ SpikeTile.java
@@ -0,0 +1,27 @@
+package game;
+/**
+ * [SpikeTile.java]
+ * This class creates a single spike tile and extends tile class
+ * @author Peter Gao, Sally Jeong
+ * @version 1.0
+ * @since June 2021
+ */
+public class SpikeTile extends Tile {
+	/**
+     * Constructs a new spike tile
+     * @param id An integer holding the id of the tile
+     */
+    public SpikeTile(int id) {
+        super(TileAssets.spike, id);
+    }
+    /**
+     * walkable
+     * This method returns if the tile is walkable
+     * @return boolean, true
+     */
+    @Override
+    public boolean walkable() {
+        return true;
+    }
+
+}
